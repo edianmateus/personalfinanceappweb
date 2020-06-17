a2edae35bb9c8009ab03bbb98beba72846ccc6c5
edian <edianmateus@hotmail.com>
2020-06-16 23:07:13 -0300

    ajustes no projeto front para receber controle de private routes com base em função

--- personalfinanceapp/src/App.js
+++ personalfinanceapp/src/App.js
@@ -1,30 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Routes from "./routes";
 
-class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={apiResponse:""};
-  }
-  callAPI(){
-    fetch('http://localhost:9000/testAPI')
-      .then(res => res.text())
-        .then(res => this.setState({apiResponse: res}));
-  }
-  componentWillMount(){
-    this.callAPI();
-  }
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <p>{this.state.apiResponse}</p>
-      </div>
-    );
-  }
-}
+const App = () => <Routes />;
 
 export default App;
